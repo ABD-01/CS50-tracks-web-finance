81b6ad29d8455b6998c053b327c0410622a54864
ABD-01 <myac931#gmail.com>
2021-01-18 13:27:39 +0000

    Changed the part of code where it requires API KEY

--- application.py
+++ application.py
@@ -39,8 +39,10 @@ Session(app)
 db = SQL("sqlite:///finance.db")
 
 # Make sure API key is set
+'''
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
+'''
 
 
 @app.route("/register", methods=["GET", "POST"])
